63bab5c213f46be27edc4ef496569c4570f30096
José Barbosa <jose.barbosa@lambda3.com.br>
2015-11-09 01:15:45 -0200

    UWP-002 - Creating your First Universal Windows Platform App

---
+++ BlankAppTemplate10/BlankAppTemplate10/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BlankAppTemplate10
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            resultTextBlock.Text = "Hello world!";
+        }
+    }
+}
